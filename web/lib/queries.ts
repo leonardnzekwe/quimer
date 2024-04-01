bc24ad93948ea8724c0b0e686c484cfda9e5e9d6
Leonard Nzekwe <nzekweleo@gmail.com>
2024-04-01 02:30:46 +0000

    Update Graphql Client and Server Configs

--- web/lib/queries.ts
+++ web/lib/queries.ts
@@ -1,15 +1,33 @@
 import { gql } from '@apollo/client';
 
 export const FETCH_USER_DASHBOARD_DATA = gql`
-  query GET_USER_DASHBOARD_DATA($id: Int) {
+query GET_USER_DASHBOARD_DATA($id: Int) {
   users(id: $id) {
     id
     username
     sessionSet { 
       id
       test { id,  title, description }
-      score
+      startTime
+      timeLimit
+      score  
     }
+  },
+  issuers {
+    id
+    name
+    description
+  },
+  tests {
+    id
+    title
+    dateCreated
+    description
+    subject { id name }
+  },
+  subjects {
+    id
+    name
   },
 }
 `;
